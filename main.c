d8694bd12bf34ce2bfd50f16b0126022db31fd91
mateuszstel <255698@edu.p.lodz.pl>
2024-12-16 09:03:56 +0100

    zmiana dodawania na mnozenie w metodzie Policz

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int policz(int a, int b) {
-    return a + b;
+    return a * b;
 }
 
 int main(void) {
     int a = 2, b = 3;
-    printf("W pierwszym branchu; 2 + 3 = %d",policz(a,b));
+    printf("W drugim branchu; 2 * 3 = %d",policz(a,b));
     return 0;
 }
